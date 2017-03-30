a761d4f66636a407d9f6d5bd0d33873fb0b74c96
Josh Pann <josh.pann@example.com>
2017-03-30 13:02:02 -0400

    ruby solution with tests

--- ruby/sentence_reverser.rb
+++ ruby/sentence_reverser.rb
@@ -1,13 +1,23 @@
 require "minitest/autorun"
+require "pry"
 
 class SentenceReverser
+
+  attr_reader :sentence
 
   def initialize(sentence)
     @sentence = sentence
   end
 
   def reverse
+    sentence_array = sentence.split(' ')
 
+    reversed_array = []
+
+    sentence_array.each do |word|
+      reversed_array.unshift(word)
+    end
+    return reversed_array.join(' ')
   end
 
 end
@@ -19,6 +29,12 @@ describe SentenceReverser do
   describe "when a sentence is given" do
     it "must reverse the sentence" do
       @sr = SentenceReverser.new('This is the best')
+      result = @sr.reverse
+      result.must_equal 'best the is This'
+    end
+
+    it "must remove whitespace and reverse the sentence" do
+      @sr = SentenceReverser.new('    This is    the    best   ')
       result = @sr.reverse
       result.must_equal 'best the is This'
     end
